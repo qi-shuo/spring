63ec1f318848cd94b86bfe2a230d29fe39a83933
qis <qi_s@qq.com>
2021-02-17 18:08:38 +0800

    spring

---
+++ spring-qis/src/main/java/com/qis/edu/TestAutowired.java
@@ -0,0 +1,11 @@
+package com.qis.edu;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qishuo
+ * @date 2021/2/14 8:52 下午
+ */
+@Component
+public class TestAutowired {
+}
---
+++ spring-qis/src/main/java/com/qis/edu/TestConfig.java
@@ -0,0 +1,34 @@
+package com.qis.edu;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qishuo1
+ * @create 2021/2/9 18:35
+ */
+@Component("testConfig")
+public class TestConfig {
+	@Autowired
+	private TestAutowired testAutowired;
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath:applicationConfig.xml");
+		TestConfig testConfig = (TestConfig) classPathXmlApplicationContext.getBean("testConfig");
+		TestConfig testConfig1 = (TestConfig) classPathXmlApplicationContext.getBean("testConfig");
+
+		System.out.println(testConfig1);
+		System.out.println(testConfig.toString());
+		System.out.println(testConfig1.equals(testConfig));
+	}
+
+	@Test
+	public void testConfig() {
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath:applicationConfig.xml");
+		TestConfig testConfig = (TestConfig) classPathXmlApplicationContext.getBean("testConfig");
+		System.out.println("-----------");
+		System.out.println(testConfig);
+	}
+}
